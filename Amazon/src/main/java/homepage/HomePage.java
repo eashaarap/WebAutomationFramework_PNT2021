dbee07b6bf933feab3621ae48ddd53fc8cfbf878
eashaarap <easha2272@gmail.com>
2021-03-14 15:41:00 -0400

    updating project to match mr hasans

--- Amazon/src/main/java/homepage/HomePage.java
+++ Amazon/src/main/java/homepage/HomePage.java
@@ -1,39 +1,75 @@
 package homepage;
 
-import org.openqa.selenium.WebDriver;
+import common.WebAPI;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 
 import static homepage.HomePageWebElement.*;
+import static homepage.HomePageWebElement.searchBoxLocator;
 
-public class HomePage {
+public class HomePage  extends WebAPI {
+    // Action Method class
 
-    //action method class
+    // Find By Annotation: First Approach
+    @FindBy (how = How.ID, using =searchBoxLocator ) public WebElement searchBox;
+    @FindBy (how = How.ID, using =searchButtonLocator ) public WebElement searchButton;
+    @FindBy (how = How.XPATH, using =searchTextLocator ) public WebElement searchText;
 
-    //Find by Annotation:
-    @FindBy(how = How.ID, using = searchBoxWebLocator)public WebElement searchBox;
-    @FindBy(how = How.ID, using = searchButtonLocator)public WebElement searchButton;
+    // Action Method
+    public void searchBox() throws InterruptedException {
+        // Enter productName
+        searchBox.sendKeys(productName);
+        Thread.sleep(3000);
+        // Click on searchButton
+        searchButton.click();
+    }
 
-//    //Find by Annotation: second approach
-//    @FindBy(id=searchBoxWebLocator)public WebElement searchBox1;
-//    //third approach
-//    @FindBy(id=searchBoxWebLocator)private WebElement searchBox2;
-//        //generate getters and setters to access
+
+    public void searchBox1(){
+        typeOnElement(searchBoxLocator,"Hand Sanitizer");
+        clickOnElement(searchButtonLocator);
+    }
+
+
+
+
+    // @FindBy (how = How.LINK_TEXT, using =searchButtonLocator ) public WebElement searchButton1;
+
+//    // Find By Annotation: Second Approach using public
+//    @FindBy(id=searchBoxLocator) public WebElement searchBox1;
+//    // Find By Annotation: Third Approach using private
+//    @FindBy(id=searchBoxLocator) private WebElement searchBox2;
+//
 //    public WebElement getSearchBox2() {
 //        return searchBox2;
 //    }
+//
 //    public void setSearchBox2(WebElement searchBox2) {
 //        this.searchBox2 = searchBox2;
 //    }
 //
-//
-//
 //    public void demo(){
 //        searchBox.sendKeys("Mask");
-//        searchBox.click();
+//        searchButton.click();
+//
 //    }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
-
-
--- Amazon/src/main/java/homepage/HomePageWebElement.java
+++ Amazon/src/main/java/homepage/HomePageWebElement.java
@@ -1,8 +1,17 @@
 package homepage;
 
+import org.openqa.selenium.By;
+
 public class HomePageWebElement {
-    //webElement class for Locators  and other properties
-    public static final String productName ="Mask";
-    public static final String searchBoxWebLocator = "twotabsearchtextbox";
-    public static final String searchButtonLocator = "nav-search-submit-button";
+    // WebElement class for Locators only and some other properties
+
+    public static final String productName="Mask";
+    public static final String searchBoxLocator="twotabsearchtextbox";
+    public static final String searchButtonLocator="nav-search-submit-button";
+    public static final String searchTextLocator="//*[@id=\"search\"]/span/div/span/h1/div/div[1]/div/div/span[3]";
+
+
+
+
 }
+
